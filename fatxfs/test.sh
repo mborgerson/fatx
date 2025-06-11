fd0989b68b094ef0fb5cc9fb3be27495868b8512
Matt Borgerson <contact@mborgerson.com>
2025-06-10 19:04:40 -0700

    fatxfs: Format test disk

--- fatxfs/test.sh
+++ fatxfs/test.sh
@@ -5,15 +5,17 @@ function checksum {
 	sha256sum $1 | cut -f1 -d' '
 }
 
-tar xvf xbox_hdd.img.tgz
 mkdir -p c
-fatxfs --log=log.txt --loglevel=100 xbox_hdd.img c
-[[ $(checksum c/xboxdash.xbe) = "338e6e203d9f1db5f2c1d976b8969af42049c32e1a65ac0347fbc6efcf5bd7c6" ]]
+
+# Format the disk
+fallocate -l 8G xbox_hdd.img
+fatxfs --format=retail --destroy-all-existing-data  xbox_hdd.img c
+sleep 1
 
 # Copy a file in
+fatxfs --log=log.txt --loglevel=100 xbox_hdd.img c
 SRC_PATH=$(which fatxfs)
 DST_PATH="c/a/b/c/d/randfile.bin"
-
 mkdir -p $(dirname $DST_PATH)
 cp $SRC_PATH $DST_PATH
 sleep 1
